21e1c0095fadcb53eb9dfec0ee5298e1e8a9ab58
Lucas Ribeiro <103015314+LuckDev22@users.noreply.github.com>
2024-10-01 21:12:34 -0300

    feat: add cors vercel website

--- src/app.ts
+++ src/app.ts
@@ -12,13 +12,15 @@ const app = express();
 const Origins = [
   "http://localhost:8888",
   "http://localhost:5173",
-  "https://capivara-catalog.vercel.app/",
-  "https://capivara-catalago-web-site.vercel.app/",
+  "https://capivara-catalog.vercel.app",
+  "https://capivara-catalago-web-site.vercel.app",
 ];
 
 app.use(
   cors({
     origin: Origins,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
   })
 );
 
